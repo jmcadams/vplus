77a6ed686c3dff7930341185d29cdeb5e8700dae
John McAdams <macebobo@gmail.com>
2013-03-17 12:14:09 -0700

    Added Resource Files

--- Plugins/UI/TriggerResponse/TriggerResponse/TriggerEditDialog.cs
+++ Plugins/UI/TriggerResponse/TriggerResponse/TriggerEditDialog.cs
@@ -1,297 +1,99 @@
-namespace TriggerResponse
-{
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Drawing;
-    using System.IO;
-    using System.Windows.Forms;
-    using Vixen;
+namespace TriggerResponse {
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Drawing;
+	using System.IO;
+	using System.Windows.Forms;
+	using Vixen;
 
-    internal class TriggerEditDialog : Form
-    {
-        private Button buttonCancel;
-        private Button buttonFindSequence;
-        private Button buttonOK;
-        private ComboBox comboBoxAudioDevice;
-        private ComboBox comboBoxLines;
-        private ComboBox comboBoxTriggers;
-        private IContainer components = null;
-        private GroupBox groupBox1;
-        private GroupBox groupBox2;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private MappedTriggerResponse m_item;
-        private string m_sequenceFile;
-        private OpenFileDialog openFileDialog;
-        private TextBox textBoxDescription;
-        private TextBox textBoxSequence;
+	internal class TriggerEditDialog : Form {
+		private MappedTriggerResponse m_item;
+		private string m_sequenceFile;
 
-        public TriggerEditDialog(List<ITriggerPlugin> triggers, string[] audioDevices)
-        {
-            this.InitializeComponent();
-            this.comboBoxTriggers.Items.AddRange(triggers.ToArray());
-            this.openFileDialog.InitialDirectory = Paths.SequencePath;
-            this.openFileDialog.Filter = ((ISystem) Interfaces.Available["ISystem"]).KnownFileTypesFilter;
-            this.comboBoxAudioDevice.Items.AddRange(audioDevices);
-            if (this.comboBoxAudioDevice.Items.Count > 0)
-            {
-                this.comboBoxAudioDevice.SelectedIndex = 0;
-            }
-        }
+		public TriggerEditDialog(List<ITriggerPlugin> triggers, string[] audioDevices) {
+			this.InitializeComponent();
+			this.comboBoxTriggers.Items.AddRange(triggers.ToArray());
+			this.openFileDialog.InitialDirectory = Paths.SequencePath;
+			this.openFileDialog.Filter = ((ISystem)Interfaces.Available["ISystem"]).KnownFileTypesFilter;
+			this.comboBoxAudioDevice.Items.AddRange(audioDevices);
+			if (this.comboBoxAudioDevice.Items.Count > 0) {
+				this.comboBoxAudioDevice.SelectedIndex = 0;
+			}
+		}
 
-        private void buttonFindSequence_Click(object sender, EventArgs e)
-        {
-            this.openFileDialog.FileName = string.Empty;
-            if (this.openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                this.textBoxSequence.Text = Path.GetFileNameWithoutExtension(this.openFileDialog.FileName);
-                this.m_sequenceFile = Path.GetFileName(this.openFileDialog.FileName);
-            }
-        }
+		private void buttonFindSequence_Click(object sender, EventArgs e) {
+			this.openFileDialog.FileName = string.Empty;
+			if (this.openFileDialog.ShowDialog() == DialogResult.OK) {
+				this.textBoxSequence.Text = Path.GetFileNameWithoutExtension(this.openFileDialog.FileName);
+				this.m_sequenceFile = Path.GetFileName(this.openFileDialog.FileName);
+			}
+		}
 
-        private void buttonOK_Click(object sender, EventArgs e)
-        {
-            if ((((this.comboBoxTriggers.SelectedIndex == -1) || (this.comboBoxLines.SelectedIndex == -1)) || (this.textBoxDescription.Text.Length == 0)) || (this.textBoxSequence.Text.Length == 0))
-            {
-                MessageBox.Show("All items are required.", "Vixen", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                base.DialogResult = System.Windows.Forms.DialogResult.None;
-            }
-            else
-            {
-                this.m_item.TriggerType = ((ITriggerPlugin) this.comboBoxTriggers.SelectedItem).Name;
-                this.m_item.TriggerIndex = (int) this.comboBoxLines.SelectedItem;
-                this.m_item.Description = this.textBoxDescription.Text;
-                this.m_item.SequenceFile = this.m_sequenceFile;
-                if (this.comboBoxAudioDevice.SelectedIndex != this.m_item.Sequence.AudioDeviceIndex)
-                {
-                    this.m_item.Sequence.AudioDeviceIndex = this.comboBoxAudioDevice.SelectedIndex;
-                    this.m_item.Sequence.Save();
-                }
-            }
-        }
+		private void buttonOK_Click(object sender, EventArgs e) {
+			if ((((this.comboBoxTriggers.SelectedIndex == -1) || (this.comboBoxLines.SelectedIndex == -1)) || (this.textBoxDescription.Text.Length == 0)) || (this.textBoxSequence.Text.Length == 0)) {
+				MessageBox.Show("All items are required.", "Vixen", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+				base.DialogResult = System.Windows.Forms.DialogResult.None;
+			}
+			else {
+				this.m_item.TriggerType = ((ITriggerPlugin)this.comboBoxTriggers.SelectedItem).Name;
+				this.m_item.TriggerIndex = (int)this.comboBoxLines.SelectedItem;
+				this.m_item.Description = this.textBoxDescription.Text;
+				this.m_item.SequenceFile = this.m_sequenceFile;
+				if (this.comboBoxAudioDevice.SelectedIndex != this.m_item.Sequence.AudioDeviceIndex) {
+					this.m_item.Sequence.AudioDeviceIndex = this.comboBoxAudioDevice.SelectedIndex;
+					this.m_item.Sequence.Save();
+				}
+			}
+		}
 
-        private void comboBoxLines_DropDown(object sender, EventArgs e)
-        {
-            if (this.comboBoxTriggers.SelectedItem == null)
-            {
-                this.comboBoxLines.Enabled = false;
-            }
-            else
-            {
-                this.QueryTriggerLines();
-            }
-        }
+		private void comboBoxLines_DropDown(object sender, EventArgs e) {
+			if (this.comboBoxTriggers.SelectedItem == null) {
+				this.comboBoxLines.Enabled = false;
+			}
+			else {
+				this.QueryTriggerLines();
+			}
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (this.components != null))
-            {
-                this.components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		private void QueryTriggerLines() {
+			this.comboBoxLines.BeginUpdate();
+			this.comboBoxLines.Items.Clear();
+			int triggerCount = ((ITriggerPlugin)this.comboBoxTriggers.SelectedItem).TriggerCount;
+			for (int i = 0; i < triggerCount; i++) {
+				this.comboBoxLines.Items.Add(i);
+			}
+			if (this.m_item != null) {
+				this.comboBoxLines.SelectedItem = this.m_item.TriggerIndex;
+			}
+			this.comboBoxLines.EndUpdate();
+			this.comboBoxLines.Enabled = true;
+		}
 
-        private void InitializeComponent()
-        {
-            this.label1 = new Label();
-            this.comboBoxTriggers = new ComboBox();
-            this.label2 = new Label();
-            this.comboBoxLines = new ComboBox();
-            this.label3 = new Label();
-            this.textBoxDescription = new TextBox();
-            this.buttonOK = new Button();
-            this.buttonCancel = new Button();
-            this.label4 = new Label();
-            this.textBoxSequence = new TextBox();
-            this.buttonFindSequence = new Button();
-            this.openFileDialog = new OpenFileDialog();
-            this.groupBox1 = new GroupBox();
-            this.groupBox2 = new GroupBox();
-            this.comboBoxAudioDevice = new ComboBox();
-            this.label5 = new Label();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            base.SuspendLayout();
-            this.label1.AutoSize = true;
-            this.label1.Location = new Point(0x12, 0x1b);
-            this.label1.Name = "label1";
-            this.label1.Size = new Size(0x3f, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Trigger type";
-            this.comboBoxTriggers.DropDownStyle = ComboBoxStyle.DropDownList;
-            this.comboBoxTriggers.FormattingEnabled = true;
-            this.comboBoxTriggers.Location = new Point(0x67, 0x18);
-            this.comboBoxTriggers.Name = "comboBoxTriggers";
-            this.comboBoxTriggers.Size = new Size(0xd0, 0x15);
-            this.comboBoxTriggers.TabIndex = 1;
-            this.label2.AutoSize = true;
-            this.label2.Location = new Point(0x152, 0x1b);
-            this.label2.Name = "label2";
-            this.label2.Size = new Size(0x1b, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Line";
-            this.comboBoxLines.DropDownStyle = ComboBoxStyle.DropDownList;
-            this.comboBoxLines.FormattingEnabled = true;
-            this.comboBoxLines.Location = new Point(0x177, 0x18);
-            this.comboBoxLines.Name = "comboBoxLines";
-            this.comboBoxLines.Size = new Size(0x35, 0x15);
-            this.comboBoxLines.TabIndex = 3;
-            this.comboBoxLines.DropDown += new EventHandler(this.comboBoxLines_DropDown);
-            this.label3.AutoSize = true;
-            this.label3.Location = new Point(0x12, 0x44);
-            this.label3.Name = "label3";
-            this.label3.Size = new Size(60, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Description";
-            this.textBoxDescription.Location = new Point(0x67, 0x41);
-            this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.Size = new Size(0xd0, 20);
-            this.textBoxDescription.TabIndex = 5;
-            this.buttonOK.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
-            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOK.Location = new Point(0x13a, 0xef);
-            this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new Size(0x4b, 0x17);
-            this.buttonOK.TabIndex = 9;
-            this.buttonOK.Text = "OK";
-            this.buttonOK.UseVisualStyleBackColor = true;
-            this.buttonOK.Click += new EventHandler(this.buttonOK_Click);
-            this.buttonCancel.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new Point(0x18b, 0xef);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new Size(0x4b, 0x17);
-            this.buttonCancel.TabIndex = 10;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            this.label4.AutoSize = true;
-            this.label4.Location = new Point(0x12, 0x22);
-            this.label4.Name = "label4";
-            this.label4.Size = new Size(0x38, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Sequence";
-            this.textBoxSequence.Location = new Point(0x67, 0x1f);
-            this.textBoxSequence.Name = "textBoxSequence";
-            this.textBoxSequence.ReadOnly = true;
-            this.textBoxSequence.Size = new Size(0x12e, 20);
-            this.textBoxSequence.TabIndex = 7;
-            this.buttonFindSequence.Location = new Point(0x194, 30);
-            this.buttonFindSequence.Name = "buttonFindSequence";
-            this.buttonFindSequence.Size = new Size(0x18, 0x15);
-            this.buttonFindSequence.TabIndex = 8;
-            this.buttonFindSequence.Text = "...";
-            this.buttonFindSequence.UseVisualStyleBackColor = true;
-            this.buttonFindSequence.Click += new EventHandler(this.buttonFindSequence_Click);
-            this.openFileDialog.Title = "Select a sequence";
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboBoxTriggers);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.comboBoxLines);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBoxDescription);
-            this.groupBox1.Location = new Point(20, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new Size(0x1bd, 0x6a);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Trigger";
-            this.groupBox2.Controls.Add(this.comboBoxAudioDevice);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.textBoxSequence);
-            this.groupBox2.Controls.Add(this.buttonFindSequence);
-            this.groupBox2.Location = new Point(20, 0x7c);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new Size(0x1bd, 0x6d);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Response";
-            this.comboBoxAudioDevice.DropDownStyle = ComboBoxStyle.DropDownList;
-            this.comboBoxAudioDevice.FormattingEnabled = true;
-            this.comboBoxAudioDevice.Location = new Point(0x67, 0x48);
-            this.comboBoxAudioDevice.Name = "comboBoxAudioDevice";
-            this.comboBoxAudioDevice.Size = new Size(0x12e, 0x15);
-            this.comboBoxAudioDevice.TabIndex = 10;
-            this.label5.AutoSize = true;
-            this.label5.Location = new Point(0x12, 0x4b);
-            this.label5.Name = "label5";
-            this.label5.Size = new Size(0x45, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Audio device";
-            base.AcceptButton = this.buttonOK;
-            base.AutoScaleDimensions = new SizeF(6f, 13f);
-            base.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            base.CancelButton = this.buttonCancel;
-            base.ClientSize = new Size(0x1e2, 0x112);
-            base.Controls.Add(this.groupBox2);
-            base.Controls.Add(this.groupBox1);
-            base.Controls.Add(this.buttonCancel);
-            base.Controls.Add(this.buttonOK);
-            base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            base.Name = "TriggerEditDialog";
-            base.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Trigger Response Edit";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            base.ResumeLayout(false);
-        }
-
-        private void QueryTriggerLines()
-        {
-            this.comboBoxLines.BeginUpdate();
-            this.comboBoxLines.Items.Clear();
-            int triggerCount = ((ITriggerPlugin) this.comboBoxTriggers.SelectedItem).TriggerCount;
-            for (int i = 0; i < triggerCount; i++)
-            {
-                this.comboBoxLines.Items.Add(i);
-            }
-            if (this.m_item != null)
-            {
-                this.comboBoxLines.SelectedItem = this.m_item.TriggerIndex;
-            }
-            this.comboBoxLines.EndUpdate();
-            this.comboBoxLines.Enabled = true;
-        }
-
-        public MappedTriggerResponse SelectedItem
-        {
-            get
-            {
-                return this.m_item;
-            }
-            set
-            {
-                this.m_item = value;
-                foreach (ITriggerPlugin plugin in this.comboBoxTriggers.Items)
-                {
-                    if (plugin.InterfaceTypeName == this.m_item.TriggerType)
-                    {
-                        this.comboBoxTriggers.SelectedItem = plugin;
-                        break;
-                    }
-                }
-                if (this.comboBoxTriggers.SelectedIndex != -1)
-                {
-                    this.QueryTriggerLines();
-                    this.textBoxDescription.Text = this.m_item.Description;
-                }
-                this.m_sequenceFile = this.m_item.SequenceFile;
-                if (this.m_sequenceFile != string.Empty)
-                {
-                    this.textBoxSequence.Text = Path.GetFileNameWithoutExtension(this.m_sequenceFile);
-                    if (this.m_item.Sequence.AudioDeviceIndex < this.comboBoxAudioDevice.Items.Count)
-                    {
-                        this.comboBoxAudioDevice.SelectedIndex = this.m_item.Sequence.AudioDeviceIndex;
-                    }
-                }
-            }
-        }
-    }
+		public MappedTriggerResponse SelectedItem {
+			get {
+				return this.m_item;
+			}
+			set {
+				this.m_item = value;
+				foreach (ITriggerPlugin plugin in this.comboBoxTriggers.Items) {
+					if (plugin.InterfaceTypeName == this.m_item.TriggerType) {
+						this.comboBoxTriggers.SelectedItem = plugin;
+						break;
+					}
+				}
+				if (this.comboBoxTriggers.SelectedIndex != -1) {
+					this.QueryTriggerLines();
+					this.textBoxDescription.Text = this.m_item.Description;
+				}
+				this.m_sequenceFile = this.m_item.SequenceFile;
+				if (this.m_sequenceFile != string.Empty) {
+					this.textBoxSequence.Text = Path.GetFileNameWithoutExtension(this.m_sequenceFile);
+					if (this.m_item.Sequence.AudioDeviceIndex < this.comboBoxAudioDevice.Items.Count) {
+						this.comboBoxAudioDevice.SelectedIndex = this.m_item.Sequence.AudioDeviceIndex;
+					}
+				}
+			}
+		}
+	}
 }
-
--- Plugins/UI/TriggerResponse/TriggerResponse/TriggerResponseTestDialog.cs
+++ Plugins/UI/TriggerResponse/TriggerResponse/TriggerResponseTestDialog.cs
@@ -1,118 +1,48 @@
-namespace TriggerResponse
-{
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Drawing;
-    using System.Windows.Forms;
-    using Vixen;
+namespace TriggerResponse {
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Drawing;
+	using System.Windows.Forms;
+	using Vixen;
 
-    internal class TriggerResponseTestDialog : Form
-    {
-        private Button buttonTest;
-        private ComboBox comboBoxTriggerResponses;
-        private IContainer components = null;
-        private Label label1;
-        private MappedTriggerResponse[] m_responsesRegistered;
-        private ITrigger m_triggerInterface;
+	internal class TriggerResponseTestDialog : Form {
+		private MappedTriggerResponse[] m_responsesRegistered;
 
-        public TriggerResponseTestDialog(MappedTriggerResponse[] responses, ITrigger triggerInterface)
-        {
-            this.InitializeComponent();
-            this.m_triggerInterface = triggerInterface;
-            List<string> list = new List<string>();
-            foreach (MappedTriggerResponse response in responses)
-            {
-                if (!list.Contains(response.Key))
-                {
-                    this.comboBoxTriggerResponses.Items.Add(response);
-                    list.Add(response.Key);
-                }
-                response.EcHandle = this.m_triggerInterface.RegisterResponse(response.TriggerType, response.TriggerIndex, response.SequenceFile);
-            }
-            this.m_responsesRegistered = responses;
-        }
+		public TriggerResponseTestDialog(MappedTriggerResponse[] responses, ITrigger triggerInterface) {
+			this.InitializeComponent();
+			this.m_triggerInterface = triggerInterface;
+			List<string> list = new List<string>();
+			foreach (MappedTriggerResponse response in responses) {
+				if (!list.Contains(response.Key)) {
+					this.comboBoxTriggerResponses.Items.Add(response);
+					list.Add(response.Key);
+				}
+				response.EcHandle = this.m_triggerInterface.RegisterResponse(response.TriggerType, response.TriggerIndex, response.SequenceFile);
+			}
+			this.m_responsesRegistered = responses;
+		}
 
-        private void buttonTest_Click(object sender, EventArgs e)
-        {
-            MappedTriggerResponse selectedItem = (MappedTriggerResponse) this.comboBoxTriggerResponses.SelectedItem;
-            this.m_triggerInterface.ActivateTrigger(selectedItem.TriggerType, selectedItem.TriggerIndex);
-        }
+		private void buttonTest_Click(object sender, EventArgs e) {
+			MappedTriggerResponse selectedItem = (MappedTriggerResponse)this.comboBoxTriggerResponses.SelectedItem;
+			this.m_triggerInterface.ActivateTrigger(selectedItem.TriggerType, selectedItem.TriggerIndex);
+		}
 
-        private void comboBoxResponses_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.buttonTest.Enabled = this.comboBoxTriggerResponses.SelectedIndex != -1;
-        }
+		private void comboBoxResponses_SelectedIndexChanged(object sender, EventArgs e) {
+			this.buttonTest.Enabled = this.comboBoxTriggerResponses.SelectedIndex != -1;
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (this.components != null))
-            {
-                this.components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		private void ResponseTestDialog_FormClosing(object sender, FormClosingEventArgs e) {
+			foreach (MappedTriggerResponse response in this.m_responsesRegistered) {
+				this.m_triggerInterface.UnregisterResponse(response.TriggerType, response.TriggerIndex, response.EcHandle);
+				response.EcHandle = 0;
+			}
+		}
 
-        private void InitializeComponent()
-        {
-            this.comboBoxTriggerResponses = new ComboBox();
-            this.buttonTest = new Button();
-            this.label1 = new Label();
-            base.SuspendLayout();
-            this.comboBoxTriggerResponses.DropDownStyle = ComboBoxStyle.DropDownList;
-            this.comboBoxTriggerResponses.FormattingEnabled = true;
-            this.comboBoxTriggerResponses.Location = new Point(0x16, 0x2a);
-            this.comboBoxTriggerResponses.Name = "comboBoxTriggerResponses";
-            this.comboBoxTriggerResponses.Size = new Size(0xf4, 0x15);
-            this.comboBoxTriggerResponses.TabIndex = 0;
-            this.comboBoxTriggerResponses.SelectedIndexChanged += new EventHandler(this.comboBoxResponses_SelectedIndexChanged);
-            this.buttonTest.Enabled = false;
-            this.buttonTest.Location = new Point(0x6d, 0x45);
-            this.buttonTest.Name = "buttonTest";
-            this.buttonTest.Size = new Size(0x4b, 0x17);
-            this.buttonTest.TabIndex = 1;
-            this.buttonTest.Text = "Test";
-            this.buttonTest.UseVisualStyleBackColor = true;
-            this.buttonTest.Click += new EventHandler(this.buttonTest_Click);
-            this.label1.AutoSize = true;
-            this.label1.Location = new Point(0x13, 0x16);
-            this.label1.Name = "label1";
-            this.label1.Size = new Size(0x76, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Triggers with responses";
-            base.AutoScaleDimensions = new SizeF(6f, 13f);
-            base.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            base.ClientSize = new Size(0x124, 0x72);
-            base.Controls.Add(this.label1);
-            base.Controls.Add(this.buttonTest);
-            base.Controls.Add(this.comboBoxTriggerResponses);
-            base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            base.KeyPreview = true;
-            base.Name = "TriggerResponseTestDialog";
-            base.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Trigger Response Test";
-            base.FormClosing += new FormClosingEventHandler(this.ResponseTestDialog_FormClosing);
-            base.KeyDown += new KeyEventHandler(this.ResponseTestDialog_KeyDown);
-            base.ResumeLayout(false);
-            base.PerformLayout();
-        }
-
-        private void ResponseTestDialog_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            foreach (MappedTriggerResponse response in this.m_responsesRegistered)
-            {
-                this.m_triggerInterface.UnregisterResponse(response.TriggerType, response.TriggerIndex, response.EcHandle);
-                response.EcHandle = 0;
-            }
-        }
-
-        private void ResponseTestDialog_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape)
-            {
-                base.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            }
-        }
-    }
+		private void ResponseTestDialog_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Escape) {
+				base.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			}
+		}
+	}
 }
-
