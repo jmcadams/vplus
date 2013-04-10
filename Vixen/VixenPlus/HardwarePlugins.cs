e69466ffe7d24f16b60179cdd5417bfb1e723265
John McAdams <macebobo@gmail.com>
2013-04-10 12:25:15 -0700

    Don't worry if a plug in directory does not exist, just skip.

--- Vixen/VixenPlus/HardwarePlugins.cs
+++ Vixen/VixenPlus/HardwarePlugins.cs
@@ -102,37 +102,30 @@ namespace VixenPlus
 			var list = new List<IHardwarePlugin>();
             //var dialog = new ProgressDialog();
             //dialog.Show();
-			foreach (string str in Directory.GetFiles(directory, "*.dll", SearchOption.TopDirectoryOnly))
-			{
-				IHardwarePlugin plugin;
-				if (!PluginCache.TryGetValue(str, out plugin))
-				{
-					try
-					{
-						Assembly assembly = Assembly.LoadFile(str);
-						foreach (Type type in assembly.GetExportedTypes())
-						{
-							foreach (Type type2 in type.GetInterfaces())
-							{
-								if (type2.Name == interfaceName)
-								{
-                                    //dialog.Message = "Loading " + Path.GetFileName(str);
-									plugin = (IHardwarePlugin) Activator.CreateInstance(type);
-									PluginCache[str] = plugin;
-								}
-							}
-						}
-					}
-					catch
-					{
-					}
-				}
-				if (plugin != null)
-				{
-					list.Add(plugin);
-				}
-			}
-            //dialog.Hide();
+		    if (Directory.Exists(directory)) {
+		        foreach (string str in Directory.GetFiles(directory, "*.dll", SearchOption.TopDirectoryOnly)) {
+		            IHardwarePlugin plugin;
+		            if (!PluginCache.TryGetValue(str, out plugin)) {
+		                try {
+		                    Assembly assembly = Assembly.LoadFile(str);
+		                    foreach (Type type in assembly.GetExportedTypes()) {
+		                        foreach (Type type2 in type.GetInterfaces()) {
+		                            if (type2.Name == interfaceName) {
+		                                //dialog.Message = "Loading " + Path.GetFileName(str);
+		                                plugin = (IHardwarePlugin) Activator.CreateInstance(type);
+		                                PluginCache[str] = plugin;
+		                            }
+		                        }
+		                    }
+		                }
+		                catch {}
+		            }
+		            if (plugin != null) {
+		                list.Add(plugin);
+		            }
+		        }
+		    }
+		    //dialog.Hide();
             //dialog.Dispose();
 			return list;
 		}
