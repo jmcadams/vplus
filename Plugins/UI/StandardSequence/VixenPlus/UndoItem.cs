6fb120815f0ed58dc7b8927cb89cf4af56ff7dca
John McAdams <macebobo@gmail.com>
2013-04-12 14:52:13 -0700

    Refactor in anticipation of adding undo type text.

--- Plugins/UI/StandardSequence/VixenPlus/UndoItem.cs
+++ Plugins/UI/StandardSequence/VixenPlus/UndoItem.cs
@@ -5,62 +5,36 @@ namespace VixenPlus
 {
     internal class UndoItem
     {
-        private readonly byte[,] _undoData;
-        private Point _location;
-        private readonly int[] _referencedChannels;
         private readonly EventSequence _sequence;
-        private readonly UndoOriginalBehavior _undoBehavior;
+
 
         public UndoItem(Point location, byte[,] data, UndoOriginalBehavior undoBehavior, EventSequence sequence, List<int> currentOrder)
         {
-            _location = location;
-            _undoData = data;
-            _undoBehavior = undoBehavior;
+            Location = location;
+            Data = data;
+            Behavior = undoBehavior;
             _sequence = sequence;
-            _referencedChannels = new int[data.GetLength(0)];
-            for (int i = 0; i < _referencedChannels.Length; i++)
+            ReferencedChannels = new int[data.GetLength(0)];
+            for (int i = 0; i < ReferencedChannels.Length; i++)
             {
-                _referencedChannels[i] = currentOrder[location.Y + i];
+                ReferencedChannels[i] = currentOrder[location.Y + i];
             }
         }
 
         public override string ToString()
         {
-            int num = _location.X * _sequence.EventPeriod;
-            return string.Format("{0} of {1} x {2} cells at {3}:{4:d2}.{5:d3}", new object[] { _undoBehavior, _undoData.GetLength(1), _undoData.GetLength(0), num / 0xea60, (num % 0xea60) / 0x3e8, num % 0x3e8 });
+            var num = Location.X * _sequence.EventPeriod;
+            return string.Format("{0} of {1} x {2} cells at {3}:{4:d2}.{5:d3}", new object[] { Behavior, Data.GetLength(1), Data.GetLength(0), num / 60000, (num % 60000) / 1000, num % 1000 });
         }
 
-        public UndoOriginalBehavior Behavior
-        {
-            get
-            {
-                return _undoBehavior;
-            }
-        }
 
-        public byte[,] Data
-        {
-            get
-            {
-                return _undoData;
-            }
-        }
+        public UndoOriginalBehavior Behavior { get; private set; }
 
-        public Point Location
-        {
-            get
-            {
-                return _location;
-            }
-        }
+        public byte[,] Data { get; private set; }
 
-        public int[] ReferencedChannels
-        {
-            get
-            {
-                return _referencedChannels;
-            }
-        }
+        public Point Location { get; private set; }
+
+        public int[] ReferencedChannels { get; private set; }
     }
 }
 
