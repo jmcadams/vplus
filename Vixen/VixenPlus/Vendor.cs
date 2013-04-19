ba73aa5d68e5fc3dce8e517b50493ad4846267c4
John McAdams <macebobo@gmail.com>
2013-04-19 15:09:59 -0700

    Added module names to the vendor class.

--- Vixen/VixenPlus/Vendor.cs
+++ Vixen/VixenPlus/Vendor.cs
@@ -1,19 +1,24 @@
 namespace VixenPlus
 {
-	public static class Vendor
-	{
-		public static string ProductDescription = "Lighting Control Software";
-		public static string ProductName = "VixenPlus";
-		public static string ProductURL = "http://www.vixenplus.com/";
+    public static class Vendor
+    {
+        public static string ProductDescription = "Lighting Control Software";
+        public static string ProductName = "Vixen Plus";
+        public static string ProductURL = "http://www.vixenplus.com/";
 
-		public static string DataExtension = "vda";
-		public static string ProfilExtension = "pro";
-		public static string ProgramExtension = "vpr";
-		public static string SequenceExtension = "vix";
+        public static string ModuleAuthoring = "Artisan";
+        public static string ModulePreview = "Rehersal";
+        public static string ModuleManager = "Roadie";
+        public static string ModuleScheduler = "Conductor";
 
-		public static string SupportURL = "http://diychristmas.org/phpbb/viewforum.php?f=34";
+        public static string DataExtension = "vda";
+        public static string ProfilExtension = "pro";
+        public static string ProgramExtension = "vpr";
+        public static string SequenceExtension = "vix";
 
-		public static string UpdateURL = "http://www.vixenplus.com/updates/";
-		public static string UpdateFile = "/ver.xml";
-	}
+        public static string SupportURL = "http://diychristmas.org/phpbb/viewforum.php?f=34";
+
+        public static string UpdateURL = "http://www.vixenplus.com/updates/";
+        public static string UpdateFile = "/ver.xml";
+    }
 }
