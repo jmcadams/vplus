0cf32a534562c3a77ed933e47a410c36636bea1d
John McAdams <macebobo@gmail.com>
2013-04-26 14:51:27 -0700

    New Audio Speed Dialog

--- Plugins/UI/StandardSequence/VixenEditor/AudioSpeedDialog.Designer.cs
+++ Plugins/UI/StandardSequence/VixenEditor/AudioSpeedDialog.Designer.cs
@@ -3,74 +3,101 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace VixenEditor
-{        
-	public partial class AudioSpeedDialog
-	{        
-		private IContainer components = null;
+namespace VixenEditor {
+    public partial class AudioSpeedDialog {
+        private IContainer components = null;
 
-		#region Windows Form Designer generated code
-		private Button buttonSet;
-private Label labelValue;
-private TrackBar trackBar;
+        #region Windows Form Designer generated code
 
-		private void InitializeComponent()
-		{
-			this.trackBar = new TrackBar();
-			this.labelValue = new Label();
-			this.buttonSet = new Button();
-			this.trackBar.BeginInit();
-			base.SuspendLayout();
-			this.trackBar.Location = new Point(12, 12);
-			this.trackBar.Maximum = 100;
-			this.trackBar.Minimum = 1;
-			this.trackBar.Name = "trackBar";
-			this.trackBar.Orientation = Orientation.Vertical;
-			this.trackBar.Size = new Size(0x2d, 240);
-			this.trackBar.TabIndex = 0;
-			this.trackBar.Value = 100;
-			this.trackBar.Scroll += new EventHandler(this.trackBar_Scroll);
-			this.labelValue.AutoSize = true;
-			this.labelValue.Font = new Font("Microsoft Sans Serif", 14f, FontStyle.Regular, GraphicsUnit.Point, 0);
-			this.labelValue.Location = new Point(0x3f, 110);
-			this.labelValue.Name = "labelValue";
-			this.labelValue.Size = new Size(0x37, 24);
-			this.labelValue.TabIndex = 1;
-			this.labelValue.Text = "100%";
-			this.buttonSet.Anchor = AnchorStyles.Left | AnchorStyles.Bottom;
-			this.buttonSet.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonSet.Location = new Point(26, 0x106);
-			this.buttonSet.Name = "buttonSet";
-			this.buttonSet.Size = new Size(0x4b, 23);
-			this.buttonSet.TabIndex = 2;
-			this.buttonSet.Text = "Set";
-			this.buttonSet.UseVisualStyleBackColor = true;
-			base.AutoScaleDimensions = new SizeF(6f, 13f);
-			base.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			base.ClientSize = new Size(0x7f, 0x129);
-			base.ControlBox = false;
-			base.Controls.Add(this.buttonSet);
-			base.Controls.Add(this.labelValue);
-			base.Controls.Add(this.trackBar);
-			base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			base.KeyPreview = true;
-			base.Name = "AudioSpeedDialog";
-			base.StartPosition = FormStartPosition.Manual;
-			this.Text = "Audio Playback Speed";
-			base.KeyPress += new KeyPressEventHandler(this.AudioSpeedDialog_KeyPress);
-			this.trackBar.EndInit();
-			base.ResumeLayout(false);
-			base.PerformLayout();
-		}
-		#endregion
+        private Button buttonSet;
+        private Label labelValue;
+        private TrackBar trackBar;
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (this.components != null))
-			{
-				this.components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-	}
+
+        private void InitializeComponent() {
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(12, 36);
+            this.trackBar.Maximum = 100;
+            this.trackBar.Minimum = 1;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(240, 45);
+            this.trackBar.TabIndex = 0;
+            this.trackBar.Value = 100;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // labelValue
+            // 
+            this.labelValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelValue.Location = new System.Drawing.Point(12, 9);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(240, 24);
+            this.labelValue.TabIndex = 1;
+            this.labelValue.Text = "Full Speed";
+            this.labelValue.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonSet.Location = new System.Drawing.Point(177, 87);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(75, 23);
+            this.buttonSet.TabIndex = 2;
+            this.buttonSet.Text = "Set Speed";
+            this.buttonSet.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(96, 86);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // AudioSpeedDialog
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.btnCancel;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(270, 124);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.buttonSet);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.trackBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(276, 130);
+            this.MinimumSize = new System.Drawing.Size(276, 130);
+            this.Name = "AudioSpeedDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AudioSpeedDialog_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected override void Dispose(bool disposing) {
+            if (disposing && (this.components != null)) {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private Button btnCancel;
+    }
 }
--- Plugins/UI/StandardSequence/VixenEditor/AudioSpeedDialog.cs
+++ Plugins/UI/StandardSequence/VixenEditor/AudioSpeedDialog.cs
@@ -8,28 +8,53 @@ namespace VixenEditor {
 		}
 
 		private void AudioSpeedDialog_KeyPress(object sender, KeyPressEventArgs e) {
-			e.Handled = true;
-			if (e.KeyChar == '\r') {
-				DialogResult = DialogResult.OK;
-			}
-			else if (e.KeyChar == '\x001b') {
-				DialogResult = DialogResult.Cancel;
-			}
+		    e.Handled = true;
+		    switch (e.KeyChar) {
+		        case '\r':
+		            DialogResult = DialogResult.OK;
+		            break;
+		        case '\x001b':
+		            DialogResult = DialogResult.Cancel;
+		            break;
+		    }
 		}
 
-		private void trackBar_Scroll(object sender, EventArgs e) {
-			labelValue.Text = trackBar.Value + @"%";
-		}
 
-		public float Rate {
+	    private void trackBar_Scroll(object sender, EventArgs e) {
+	        SetText();
+	    }
+
+
+	    private void SetText() {
+	        var text = trackBar.Value + @"%";
+	        switch (text) {
+	            case "25%":
+	                text = "Quarter Speed";
+	                break;
+	            case "50%":
+	                text = "Half Speed";
+	                break;
+	            case "75%":
+	                text = "Three-Quarter Speed";
+	                break;
+	            case "100%":
+	                text = "Full Speed";
+	                break;
+	        }
+	        labelValue.Text = text;
+	    }
+
+
+	    public float Rate {
 			get {
 				return (trackBar.Value / 100f);
 			}
 			set {
-				if ((value > 0f) && (value <= 1f)) {
-					trackBar.Value = (int)(value * 100f);
-					labelValue.Text = trackBar.Value + @"%";
-				}
+			    if ((!(value > 0f)) || (!(value <= 1f))) {
+			        return;
+			    }
+			    trackBar.Value = (int)(value * 100f);
+			    SetText();
 			}
 		}
 	}
