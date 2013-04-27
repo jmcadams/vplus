43a7201a824a623b709e371450a154b04633607e
John McAdams <macebobo@gmail.com>
2013-04-26 20:31:18 -0700

    Code cleanup

--- Plugins/UI/StandardSequence/VixenEditor/TestConsoleDialog.cs
+++ Plugins/UI/StandardSequence/VixenEditor/TestConsoleDialog.cs
@@ -2,11 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
+using Properties;
+
 using VixenPlus;
 
 namespace VixenEditor {
-
-    //TODO: Convert this to simple renard tester
 
     internal partial class TestConsoleDialog : Form {
         private readonly List<ConsoleTrackBar> _channelControls = new List<ConsoleTrackBar>();
@@ -14,6 +14,7 @@ namespace VixenEditor {
         private readonly Channel[] _channels;
         private readonly int _executionContextHandle;
         private readonly IExecution _executionInterface;
+
 
         public TestConsoleDialog(EventSequence sequence, IExecution executionInterface) {
             InitializeComponent();
@@ -23,55 +24,62 @@ namespace VixenEditor {
             _channelLevels = new byte[sequence.ChannelCount];
             _channels = sequence.Channels.ToArray();
             var strArray = new string[sequence.ChannelCount + 1];
-            for (int i = 1; i <= sequence.ChannelCount; i++) {
-                strArray[i] = sequence.Channels[i - 1].Name;
+            for (var channel = 1; channel <= sequence.ChannelCount; channel++) {
+                strArray[channel] = sequence.Channels[channel - 1].Name;
             }
-            strArray[0] = " -- unassigned -- ";
-            foreach (Control control in groupBox2.Controls) {
+            strArray[0] = Resources.Unassigned;
+            foreach (var control in groupBox2.Controls) {
                 var item = control as ConsoleTrackBar;
-                if ((item != null) &&
-                    (item.Master != null)) {
-                    item.TextStrings = strArray;
-                    item.ResetIndex = 0;
-                    _channelControls.Add(item);
+                if ((item == null) || (item.Master == null)) {
+                    continue;
                 }
+
+                item.TextStrings = strArray;
+                item.ResetIndex = 0;
+                _channelControls.Add(item);
             }
         }
+
 
         private void buttonDone_Click(object sender, EventArgs e) {
             Close();
         }
 
+
         private void consoleTrackBar_ValueChanged(object sender) {
             var bar = sender as ConsoleTrackBar;
-            if (bar != null &&
-                bar.SelectedTextIndex > 0) {
-                UpdateChannelFrom(sender as ConsoleTrackBar);
-                UpdateOutput();
+            if (bar == null || bar.SelectedTextIndex <= 0) {
+                return;
             }
+            UpdateChannelFrom(sender as ConsoleTrackBar);
+            UpdateOutput();
         }
 
+
         private void consoleTrackBarMaster_ValueChanged(object sender) {
-            foreach (var bar in _channelControls) {
-                UpdateChannelFrom(bar);
+            foreach (var trackBar in _channelControls) {
+                UpdateChannelFrom(trackBar);
             }
             UpdateOutput();
         }
+
 
         private void TestConsoleDialog_FormClosing(object sender, FormClosingEventArgs e) {
             _executionInterface.ReleaseContext(_executionContextHandle);
         }
 
+
         private void UpdateChannelFrom(ConsoleTrackBar consoleTrackBar) {
-            if ((consoleTrackBar != null) &&
-                (consoleTrackBar.SelectedTextIndex > 0)) {
+            if ((consoleTrackBar != null) && (consoleTrackBar.SelectedTextIndex > 0)) {
                 UpdateChannelValue(consoleTrackBar.SelectedTextIndex - 1, (byte) consoleTrackBar.Value);
             }
         }
 
+
         private void UpdateChannelValue(int channelNaturalIndex, byte value) {
             _channelLevels[_channels[channelNaturalIndex].OutputChannel] = value;
         }
+
 
         private void UpdateOutput() {
             _executionInterface.SetChannelStates(_executionContextHandle, _channelLevels);
