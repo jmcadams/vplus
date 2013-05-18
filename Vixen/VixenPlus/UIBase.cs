4d5206892cffffeee1c98c9ac710e24d3a626a6e
John McAdams <macebobo@gmail.com>
2013-05-18 12:54:14 -0700

    Removed commented out code.

--- Vixen/VixenPlus/UIBase.cs
+++ Vixen/VixenPlus/UIBase.cs
@@ -1,93 +1,73 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace VixenPlus
-{
-//    [TypeDescriptionProvider(typeof(GeneralConcreteClassProvider)), ConcreteClass(typeof(UIBaseConcreteForm))]
-    public partial class UIBase : Form, IUIPlugIn
-    {
+namespace VixenPlus {
+    public partial class UIBase : Form, IUIPlugIn {
         private bool _isDirty;
 
-        public UIBase()
-        {
+
+        public UIBase() {
             InitializeComponent();
         }
 
-        public virtual /*abstract*/ EventSequence New()
-        {
+
+        public virtual EventSequence New() {
             throw new NotImplementedException();
         }
 
-        public virtual /*abstract*/ EventSequence New(EventSequence seedSequence)
-        {
+
+        public virtual EventSequence New(EventSequence seedSequence) {
             throw new NotImplementedException();
         }
 
-        public virtual /*abstract*/ void Notify(Notification notification, object data)
-        {
+
+        public virtual void Notify(Notification notification, object data) {
             throw new NotImplementedException();
         }
 
-        public virtual /*abstract*/ EventSequence Open(string filePath)
-        {
+
+        public virtual EventSequence Open(string filePath) {
             throw new NotImplementedException();
         }
 
-        public virtual /*abstract*/ DialogResult RunWizard(ref EventSequence resultSequence)
-        {
+
+        public virtual DialogResult RunWizard(ref EventSequence resultSequence) {
             throw new NotImplementedException();
         }
 
-        public virtual /*abstract*/ void SaveTo(string filePath)
-        {
+
+        public virtual void SaveTo(string filePath) {
             throw new NotImplementedException();
         }
 
-        //string IPlugIn.get_Name()
-        //{
-        //    return base.Name;
-        //}
 
-        //Form IUIPlugIn.get_MdiParent()
-        //{
-        //    return base.MdiParent;
-        //}
-
-        //void IUIPlugIn.set_MdiParent(Form form1)
-        //{
-        //    base.MdiParent = form1;
-        //}
-
-        void IUIPlugIn.Show()
-        {
+        void IUIPlugIn.Show() {
             Show();
         }
 
-        public virtual /*abstract*/ string Author { get; set; }
 
-        public virtual /*abstract*/ string Description { get; set; }
+        public virtual string Author { get; set; }
 
-        public virtual /*abstract*/ string FileExtension { get; set; }
+        public virtual string Description { get; set; }
 
-        public virtual /*abstract*/ string FileTypeDescription { get; set; }
+        public virtual string FileExtension { get; set; }
 
-        public bool IsDirty
-        {
+        public virtual string FileTypeDescription { get; set; }
+
+        public bool IsDirty {
             get { return _isDirty; }
-            set
-            {
+            set {
                 _isDirty = value;
                 OnDirtyChanged(EventArgs.Empty);
             }
         }
 
-        public virtual /*abstract*/ EventSequence Sequence { get; set; }
+        public virtual EventSequence Sequence { get; set; }
         public event EventHandler DirtyChanged;
 
-        public virtual void OnDirtyChanged(EventArgs e)
-        {
-            if (DirtyChanged != null)
-            {
+
+        public virtual void OnDirtyChanged(EventArgs e) {
+            if (DirtyChanged != null) {
                 DirtyChanged(this, e);
             }
         }
