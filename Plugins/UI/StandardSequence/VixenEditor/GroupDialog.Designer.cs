dd8cf32bc2ce6a0e949b3313abcad859dc74b00a
John McAdams <macebobo@gmail.com>
2013-06-01 15:45:36 -0700

    Added initial group dialog box for building and managing groups.

---
+++ Plugins/UI/StandardSequence/VixenEditor/GroupDialog.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace VixenEditor
+{
+    partial class GroupDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GroupDialog));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lbChannels = new System.Windows.Forms.ListBox();
+            this.tvGroups = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(555, 451);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(474, 451);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lbChannels
+            // 
+            this.lbChannels.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.lbChannels.FormattingEnabled = true;
+            this.lbChannels.IntegralHeight = false;
+            this.lbChannels.Location = new System.Drawing.Point(12, 12);
+            this.lbChannels.Name = "lbChannels";
+            this.lbChannels.Size = new System.Drawing.Size(156, 433);
+            this.lbChannels.TabIndex = 4;
+            this.lbChannels.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lbChannels_DrawItem);
+            // 
+            // tvGroups
+            // 
+            this.tvGroups.Location = new System.Drawing.Point(355, 12);
+            this.tvGroups.Name = "tvGroups";
+            this.tvGroups.Size = new System.Drawing.Size(275, 433);
+            this.tvGroups.TabIndex = 7;
+            // 
+            // GroupDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 484);
+            this.Controls.Add(this.tvGroups);
+            this.Controls.Add(this.lbChannels);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GroupDialog";
+            this.Text = "GroupDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListBox lbChannels;
+        private System.Windows.Forms.TreeView tvGroups;
+    }
+}
---
+++ Plugins/UI/StandardSequence/VixenEditor/GroupDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+using VixenPlus;
+
+namespace VixenEditor
+{
+    public partial class GroupDialog: Form
+    {
+        private readonly List<Channel> _channels;
+
+        public GroupDialog(EventSequence sequence, bool constrainToGroup)
+        {
+            InitializeComponent();
+            _channels = constrainToGroup ? sequence.Channels : sequence.FullChannels; 
+            foreach (var c in _channels) {
+                lbChannels.Items.Add(c.Name);
+            }
+            if (sequence.Groups == null) {
+                return;
+            }
+            foreach (var g in sequence.Groups) {
+                var thisNode = tvGroups.Nodes.Add(g.Key);
+                thisNode.Nodes.Add(g.Value.GroupChannels);
+            }
+        }
+
+        private void lbChannels_DrawItem(object sender, DrawItemEventArgs e) {
+            Channel.DrawItem(e, _channels[e.Index]);
+        }
+    }
+}
