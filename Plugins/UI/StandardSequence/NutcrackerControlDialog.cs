a8334c04623414f760ede6fc30416b9663bd2542
John McAdams <macebobo@gmail.com>
2013-06-08 23:31:15 -0700

    Adding Nutcracker Initial Dialog

---
+++ Plugins/UI/StandardSequence/NutcrackerControlDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VixenEditor
+{
+	public partial class NutcrackerControlDialog: Form
+	{
+		public NutcrackerControlDialog()
+		{
+			InitializeComponent();
+		}
+	}
+}
