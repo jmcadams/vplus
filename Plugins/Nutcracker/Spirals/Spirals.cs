15830db5763188fb89e63f48ad98a2c0b9baaf37
John McAdams <john@rosefish.com>
2013-06-17 13:15:53 -0700

    Minor refactor

--- Plugins/Nutcracker/Spirals/Spirals.cs
+++ Plugins/Nutcracker/Spirals/Spirals.cs
@@ -21,8 +21,8 @@ namespace Spirals {
         public Color[,] RenderEffect(Color[,] buffer, Color[] palette, int eventToRender) {
             var colorCount = palette.Length;
             var spiralCount = colorCount * tbPaletteRepeat.Value;
-            var bufferWidth = buffer.GetLength(Utils.IndexColsOrWidth);
             var bufferHeight = buffer.GetLength(Utils.IndexRowsOrHeight);
+            var bufferWidth = buffer.GetLength(Utils.IndexColsOrWidth);
             var deltaStrands = bufferWidth / spiralCount;
             var spiralThickness = (deltaStrands * tbThickness.Value / 100) + 1;
             long spiralState = eventToRender * tbDirection.Value;
@@ -30,8 +30,8 @@ namespace Spirals {
             for (var spiral = 0; spiral < spiralCount; spiral++) {
                 var strandBase = spiral * deltaStrands;
                 var color = palette[spiral % colorCount];
-                for (var thick = 0; thick < spiralThickness; thick++) {
-                    var strand = (strandBase + thick) % bufferWidth;
+                for (var thickness = 0; thickness < spiralThickness; thickness++) {
+                    var strand = (strandBase + thickness) % bufferWidth;
                     for (var row = 0; row < bufferHeight; row++) {
                         var column = (strand + ((int)spiralState / 10) + (row * tbRotations.Value / bufferHeight)) % bufferWidth;
                         if (column < 0) {
@@ -42,12 +42,7 @@ namespace Spirals {
                         }
                         if (chkBox3D.Checked) {
                             var hsv = HSVUtils.ColorToHSV(color);
-                            if (tbRotations.Value < 0) {
-                                hsv.Value = (float) ((double) (thick + 1) / spiralThickness);
-                            }
-                            else {
-                                hsv.Value = (float) ((double) (spiralThickness - thick) / spiralThickness);
-                            }
+                            hsv.Value = (float)((double)(tbRotations.Value < 0 ? thickness + 1 : spiralThickness - thickness) / spiralThickness);
                             color = HSVUtils.HSVtoColor(hsv);
                         }
                         buffer[row, column] = color;
