9ae89410cb5d4d1067091c5dca478f7236135ab7
John McAdams <macebobo@gmail.com>
2013-06-18 14:44:20 -0700

    Snowstorm Done.

--- Plugins/Nutcracker/Snowstorm/Snowstorm.Designer.cs
+++ Plugins/Nutcracker/Snowstorm/Snowstorm.Designer.cs
@@ -23,24 +23,24 @@ namespace Snowstorm {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.tbSpacing = new System.Windows.Forms.TrackBar();
+            this.tbTailLength = new System.Windows.Forms.TrackBar();
             this.lblTrailLength = new System.Windows.Forms.Label();
-            this.tbGarlandType = new System.Windows.Forms.TrackBar();
+            this.tbMaxFlakes = new System.Windows.Forms.TrackBar();
             this.lblMaxFlakes = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.tbSpacing)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tbGarlandType)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbTailLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbMaxFlakes)).BeginInit();
             this.SuspendLayout();
             // 
-            // tbSpacing
+            // tbTailLength
             // 
-            this.tbSpacing.AutoSize = false;
-            this.tbSpacing.Location = new System.Drawing.Point(90, 34);
-            this.tbSpacing.Maximum = 100;
-            this.tbSpacing.Name = "tbSpacing";
-            this.tbSpacing.Size = new System.Drawing.Size(139, 25);
-            this.tbSpacing.TabIndex = 17;
-            this.tbSpacing.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.tbSpacing.ValueChanged += new System.EventHandler(this.SnowStorm_ControlChanged);
+            this.tbTailLength.AutoSize = false;
+            this.tbTailLength.Location = new System.Drawing.Point(90, 34);
+            this.tbTailLength.Maximum = 100;
+            this.tbTailLength.Name = "tbTailLength";
+            this.tbTailLength.Size = new System.Drawing.Size(139, 25);
+            this.tbTailLength.TabIndex = 17;
+            this.tbTailLength.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.tbTailLength.ValueChanged += new System.EventHandler(this.SnowStorm_ControlChanged);
             // 
             // lblTrailLength
             // 
@@ -51,16 +51,16 @@ namespace Snowstorm {
             this.lblTrailLength.TabIndex = 16;
             this.lblTrailLength.Text = "Trail Length";
             // 
-            // tbGarlandType
+            // tbMaxFlakes
             // 
-            this.tbGarlandType.AutoSize = false;
-            this.tbGarlandType.Location = new System.Drawing.Point(90, 3);
-            this.tbGarlandType.Maximum = 100;
-            this.tbGarlandType.Name = "tbGarlandType";
-            this.tbGarlandType.Size = new System.Drawing.Size(139, 25);
-            this.tbGarlandType.TabIndex = 15;
-            this.tbGarlandType.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.tbGarlandType.ValueChanged += new System.EventHandler(this.SnowStorm_ControlChanged);
+            this.tbMaxFlakes.AutoSize = false;
+            this.tbMaxFlakes.Location = new System.Drawing.Point(90, 3);
+            this.tbMaxFlakes.Maximum = 100;
+            this.tbMaxFlakes.Name = "tbMaxFlakes";
+            this.tbMaxFlakes.Size = new System.Drawing.Size(139, 25);
+            this.tbMaxFlakes.TabIndex = 15;
+            this.tbMaxFlakes.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.tbMaxFlakes.ValueChanged += new System.EventHandler(this.SnowStorm_ControlChanged);
             // 
             // lblMaxFlakes
             // 
@@ -75,14 +75,14 @@ namespace Snowstorm {
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tbSpacing);
+            this.Controls.Add(this.tbTailLength);
             this.Controls.Add(this.lblTrailLength);
-            this.Controls.Add(this.tbGarlandType);
+            this.Controls.Add(this.tbMaxFlakes);
             this.Controls.Add(this.lblMaxFlakes);
             this.Name = "Snowstorm";
             this.Size = new System.Drawing.Size(232, 134);
-            ((System.ComponentModel.ISupportInitialize)(this.tbSpacing)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.tbGarlandType)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbTailLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbMaxFlakes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,9 +90,9 @@ namespace Snowstorm {
 
         #endregion
 
-        private System.Windows.Forms.TrackBar tbSpacing;
+        private System.Windows.Forms.TrackBar tbTailLength;
         private System.Windows.Forms.Label lblTrailLength;
-        private System.Windows.Forms.TrackBar tbGarlandType;
+        private System.Windows.Forms.TrackBar tbMaxFlakes;
         private System.Windows.Forms.Label lblMaxFlakes;
     }
 }
--- Plugins/Nutcracker/Snowstorm/Snowstorm.cs
+++ Plugins/Nutcracker/Snowstorm/Snowstorm.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-
+using CommonUtils;
 using VixenPlus;
 
 namespace Snowstorm {
@@ -10,17 +11,199 @@ namespace Snowstorm {
             InitializeComponent();
         }
 
+
         public event EventHandler OnControlChanged;
+        private readonly Random _random = new Random();
 
         public string EffectName {
             get { return "Snow Storm"; }
         }
 
-        public Color[,] RenderEffect(Color[,] buffer, Color[] palette, int eventToRender) {
-            throw new NotImplementedException();
+        private class SnowstormClass {
+            public readonly List<Point> Points = new List<Point>();
+            public HSVUtils HSV;
+            public int Idx, SsDecay;
+
+
+            public SnowstormClass() {
+                Points.Clear();
+            }
         }
 
+        private readonly List<SnowstormClass> _snowstormItems = new List<SnowstormClass>();
+        private int _bufferHeight;
+        private int _bufferWidth;
+        private Color[] _palette;
+
+
+        private void InitializeSnowstorm() {
+            if (_palette == null) return;
+
+            var hsv0 = HSVUtils.ColorToHSV(_palette[0]);
+            var hsv1 = HSVUtils.ColorToHSV(_palette[1]);
+
+            var count = Convert.ToInt32(_bufferWidth * _bufferHeight * tbMaxFlakes.Value / 2000) + 1;
+            var tailLength = _bufferWidth * _bufferHeight * tbTailLength.Value / 2000 + 2;
+            var xy = new Point();
+            // create snowstorm elements
+            _snowstormItems.Clear();
+            for (var i = 0; i < count; i++) {
+                var ssItem = new SnowstormClass {Idx = i, SsDecay = 0};
+                ssItem.Points.Clear();
+                ssItem.HSV = HSVUtils.SetRangeColor(hsv0, hsv1);
+                // start in a random state
+                var r = Rand() % (2 * tailLength);
+                if (r > 0) {
+                    xy.X = Rand() % _bufferWidth;
+                    xy.Y = Rand() % _bufferHeight;
+                    //ssItem.points.push_back(xy);
+                    ssItem.Points.Add(xy);
+                }
+                if (r >= tailLength) {
+                    ssItem.SsDecay = r - tailLength;
+                    r = tailLength;
+                }
+                for (var j = 1; j < r; j++) {
+                    SnowstormAdvance(ssItem);
+                }
+                //SnowstormItems.push_back(ssItem);
+                _snowstormItems.Add(ssItem);
+            }
+        }
+
+
+        private int lastRenderedEvent = -1;
+        
+        public Color[,] RenderEffect(Color[,] buffer, Color[] palette, int eventToRender) {
+            _bufferHeight = buffer.GetLength(Utils.IndexRowsOrHeight);
+            _bufferWidth = buffer.GetLength(Utils.IndexColsOrWidth);
+            _palette = palette;
+            var speed = eventToRender - lastRenderedEvent;
+            lastRenderedEvent = eventToRender;
+            var tailLength = _bufferWidth * _bufferHeight * tbTailLength.Value / 2000 + 2;
+            var xy = new Point();
+            if (eventToRender == 0) {
+                InitializeSnowstorm();
+            }
+
+            foreach (var it in _snowstormItems) {
+                if (it.Points.Count > tailLength) {
+                    if (it.SsDecay > tailLength) {
+                        it.Points.Clear(); // start over
+                        it.SsDecay = 0;
+                    }
+                    else if (Rand() % 20 < speed) {
+                        it.SsDecay++;
+                    }
+                }
+                if (it.Points.Count == 0) {
+                    xy.X = Rand() % _bufferWidth;
+                    xy.Y = Rand() % _bufferHeight;
+                    //it.points.push_back(xy);
+                    it.Points.Add(xy);
+                }
+                else if (Rand() % 20 < speed) {
+                    SnowstormAdvance(it);
+                }
+                var sz = it.Points.Count;
+                for (var pt = 0; pt < sz; pt++) {
+                    var hsv = it.HSV;
+                    hsv.Value = (float) (1.0 - (double) (sz - pt + it.SsDecay) / tailLength);
+                    if (hsv.Value < 0.0) {
+                        hsv.Value = 0.0f;
+                    }
+                    buffer[it.Points[pt].Y, it.Points[pt].X] = HSVUtils.HSVtoColor(hsv); // (it.points[pt].X, it.points[pt].Y, hsv);
+                }
+                //cnt++;
+            }
+
+            return buffer;
+        }
+
+
+        private int Rand() {
+            return _random.Next();
+        }
+
+
+        private Point SnowstormVector(int idx) {
+            var xy = new Point();
+            switch (idx) {
+                case 0:
+                    xy.X = -1;
+                    xy.Y = 0;
+                    break;
+                case 1:
+                    xy.X = -1;
+                    xy.Y = -1;
+                    break;
+                case 2:
+                    xy.X = 0;
+                    xy.Y = -1;
+                    break;
+                case 3:
+                    xy.X = 1;
+                    xy.Y = -1;
+                    break;
+                case 4:
+                    xy.X = 1;
+                    xy.Y = 0;
+                    break;
+                case 5:
+                    xy.X = 1;
+                    xy.Y = 1;
+                    break;
+                case 6:
+                    xy.X = 0;
+                    xy.Y = 1;
+                    break;
+                default:
+                    xy.X = -1;
+                    xy.Y = 1;
+                    break;
+            }
+            return xy;
+        }
+
+
+        private void SnowstormAdvance(SnowstormClass ssItem) {
+            const int cnt = 8; // # of integers in each set in arr[]
+            int[] arr = {30, 20, 10, 5, 0, 5, 10, 20, 20, 15, 10, 10, 10, 10, 10, 15}; // 2 sets of 8 numbers, each of which add up to 100
+            var adv = SnowstormVector(7);
+            var i0 = ssItem.Idx % 7 <= 4 ? 0 : cnt;
+            var r = Rand() % 100;
+            for (int i = 0, val = 0; i < cnt; i++) {
+                val += arr[i0 + i];
+                if (r >= val) {
+                    continue;
+                }
+                adv = SnowstormVector(i);
+                break;
+            }
+            if (ssItem.Idx % 3 == 0) {
+                adv.X *= 2;
+                adv.Y *= 2;
+            }
+            //Point xy = ssItem.points.back() + adv;
+            var xy = ssItem.Points[ssItem.Points.Count - 1];
+            xy.X += adv.X;
+            xy.Y += adv.Y;
+
+            xy.X %= _bufferWidth;
+            xy.Y %= _bufferHeight;
+            if (xy.X < 0) {
+                xy.X += _bufferWidth;
+            }
+            if (xy.Y < 0) {
+                xy.Y += _bufferHeight;
+            }
+            //ssItem.points.push_back(xy);
+            ssItem.Points.Add(xy);
+        }
+
+
         private void SnowStorm_ControlChanged(object sender, EventArgs e) {
+            InitializeSnowstorm();
             OnControlChanged(this, new EventArgs());
         }
     }
