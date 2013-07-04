63a2c89d108a0959bc595fe6736b3b615fe14e17
John McAdams <macebobo@gmail.com>
2013-07-04 11:01:52 -0700

    Updated release notes and version # for 0.0.184.1

--- Vixen/AssemblyInfo.cs
+++ Vixen/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.182.2")]
+[assembly: AssemblyVersion("0.0.184.1")]
 [assembly: CompilationRelaxations(8)]
-[assembly: AssemblyFileVersion("0.0.182.2")]
+[assembly: AssemblyFileVersion("0.0.184.1")]
 [assembly:
     Debuggable(
         DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.EnableEditAndContinue |
