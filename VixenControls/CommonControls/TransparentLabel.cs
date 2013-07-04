ae7959789d782a89d20e5233cd2188670fe741c8
John McAdams <macebobo@gmail.com>
2013-07-04 10:10:43 -0700

    Added a transparent label since it was not allowing a double click when it covered a cell.  Whew!

---
+++ VixenControls/CommonControls/TransparentLabel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace CommonControls
+{
+    public partial class TransparentLabel: Label
+    {	
+        public TransparentLabel()
+        {
+            InitializeComponent();
+        }
+
+        public TransparentLabel(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m) {
+            const int wmNchittest = 0x0084;
+            const int httransparent = (-1);
+
+            if (m.Msg == wmNchittest) {
+                m.Result = (IntPtr)httransparent;
+            }
+            else {
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
