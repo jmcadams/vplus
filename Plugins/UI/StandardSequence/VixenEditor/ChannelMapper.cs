116e3115dcbdcdea347088672d7afd3d77e6106b
John McAdams <macebobo@gmail.com>
2013-09-21 12:46:01 -0700

    Added channel mapper initial checkin

---
+++ Plugins/UI/StandardSequence/VixenEditor/ChannelMapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace VixenEditor {
+    public partial class ChannelMapper : Form {
+        public ChannelMapper() {
+            InitializeComponent();
+        }
+    }
+}
