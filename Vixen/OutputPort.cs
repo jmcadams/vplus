07492617f58d77a2a17676c0fbd8aab556cffa5d
John McAdams <macebobo@gmail.com>
2014-01-26 14:27:25 -0800

    Move some stuff back out that was moved by accident.

---
+++ Vixen/OutputPort.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+internal class OutputPort
+{
+    public int Index;
+    public bool IsExpanded;
+    public string Name;
+    public List<IHardwarePlugin> ReferencingPlugins = new List<IHardwarePlugin>();
+    public bool Shared;
+    public string StringFormat;
+
+    public OutputPort(string name, int index, bool shared, string stringFormat)
+    {
+        Name = name;
+        Index = index;
+        Shared = shared;
+        StringFormat = stringFormat;
+    }
+}
