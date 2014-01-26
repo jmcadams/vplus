eddd367b17141f5f2d75dc3e0d16e56a234d1244
John McAdams <macebobo@gmail.com>
2014-01-26 14:40:02 -0800

    Some resharper cleanup

--- Vixen/EngineContext.cs
+++ Vixen/EngineContext.cs
@@ -4,18 +4,18 @@ using FMOD;
 
 internal class EngineContext
 {
-    public byte[] ChannelMask = null;
-    public EventSequence CurrentSequence = null;
+    public byte[] ChannelMask;
+    public EventSequence CurrentSequence;
     public byte[,] Data;
     public int FadeStartTickCount;
     public int LastIndex = -1;
-    public byte[] LastPeriod = null;
+    public byte[] LastPeriod;
     public int MaxEvent = 2147483647;
     public RouterContext RouterContext;
-    public int SequenceIndex = 0;
+    public int SequenceIndex;
     public int SequenceTickLength;
     public SoundChannel SoundChannel;
-    public int StartOffset = 0;
-    public int TickCount = 0;
-    public Stopwatch Timekeeper = new Stopwatch();
+    public int StartOffset;
+    public int TickCount;
+    public readonly Stopwatch Timekeeper = new Stopwatch();
 }
