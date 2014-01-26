9aa6351530cab230358efe25cf6d2e012ad6e65b
John McAdams <macebobo@gmail.com>
2014-01-25 19:40:28 -0800

    Added Flags attribute to MODE enum

--- fmod/FMOD/fmod.cs
+++ fmod/FMOD/fmod.cs
@@ -750,6 +750,7 @@ namespace FMOD
         Channel::set3DCustomRolloff
     ]
     */
+    [Flags]
     public enum MODE :uint
     {
         DEFAULT                = 0x00000000,  /* FMOD_DEFAULT is a default sound type.  Equivalent to all the defaults listed below.  FMOD_LOOP_OFF, FMOD_2D, FMOD_HARDWARE. */
