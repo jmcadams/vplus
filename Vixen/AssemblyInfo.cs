1c709fa1f5ce88277841906f1f540f2ebcfa5cca
John McAdams <macebobo@gmail.com>
2014-02-10 06:11:03 -0800

    Fixed versioning issue

--- Vixen/AssemblyInfo.cs
+++ Vixen/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("V 0.2.41.1")]
+[assembly: AssemblyVersion("0.2.41.1")]
 [assembly: CompilationRelaxations(8)]
-[assembly: AssemblyFileVersion("V 0.2.41.1")]
+[assembly: AssemblyFileVersion("0.2.41.1")]
 [assembly:
     Debuggable(
         DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.EnableEditAndContinue |
