85f5bb49c92c3dadd555792709d0dd636ee66b04
John McAdams <macebobo@gmail.com>
2014-05-24 16:21:44 -0700

    New Release Version

--- Vixen/AssemblyInfo.cs
+++ Vixen/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.2.314.1357")]
+[assembly: AssemblyVersion("0.2.144.1")]
 [assembly: CompilationRelaxations(8)]
-[assembly: AssemblyFileVersion("0.2.314.1357")]
+[assembly: AssemblyFileVersion("0.2.144.1")]
 [assembly:
     Debuggable(
         DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.EnableEditAndContinue |
