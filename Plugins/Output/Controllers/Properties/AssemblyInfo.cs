33427f7c1205b66807e2fd73704d0ad7891dc6e2
John McAdams <macebobo@gmail.com>
2014-05-26 22:12:03 -0700

    Trying to get the automatic deployment working.  Will not build controllers for some reason in Release32 mode.

--- Plugins/Output/Controllers/Properties/AssemblyInfo.cs
+++ Plugins/Output/Controllers/Properties/AssemblyInfo.cs
@@ -1,35 +1,21 @@
-﻿using System.Reflection;
+﻿// Assembly Controllers, Version 1.0.0.0
+
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Controllers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Controllers")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyVersion("0.2.144.1")]
+[assembly: AssemblyCopyright("Copyleft 2013")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyFileVersion("0.2.144.1")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("VixenPlus")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Vixen+ Lighting Control")]
+[assembly: AssemblyTitle("VixenPlus")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.EnableEditAndContinue | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.Default)]
+[assembly: CompilationRelaxations(8)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e210dac0-817a-44bc-987d-fa82f064d86a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
