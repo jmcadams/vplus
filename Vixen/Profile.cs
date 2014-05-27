b2313e52bcc3eb0ff03faf8348d1fb091f30c4fe
John McAdams <macebobo@gmail.com>
2014-05-27 08:49:57 -0700

    Fixed VIX-28 - null group was being passed into profile.

--- Vixen/Profile.cs
+++ Vixen/Profile.cs
@@ -66,7 +66,12 @@ namespace VixenPlus {
 
         public string FileName { get; set; }
 
-        public Dictionary<string, GroupData> Groups { get; set; }
+        private Dictionary<string, GroupData> _groups;
+
+        public Dictionary<string, GroupData> Groups {
+            get { return _groups ?? new Dictionary<string, GroupData>(); }
+            set { _groups = value; }
+        }
 
         public bool IsDirty { get; set; }
 
