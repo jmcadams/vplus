e5beea80e83b85e36e782ae71da5733cb7b23994
John McAdams <john@rosefish.com>
2014-05-28 15:47:44 -0700

    Updated version number.

--- Plugins/UI/StandardSequence/AssemblyInfo.cs
+++ Plugins/UI/StandardSequence/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.2.146.1")]
-[assembly: AssemblyFileVersion("0.2.146.1")]
+[assembly: AssemblyVersion("0.2.148.1")]
+[assembly: AssemblyFileVersion("0.2.148.1")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 [assembly: ComVisible(false)]
 [assembly: AssemblyTrademark("")]
