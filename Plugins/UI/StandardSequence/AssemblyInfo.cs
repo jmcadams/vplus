a827bdd8cb4f2dc9085930dd95f3eb60ea834fbf
John McAdams <macebobo@gmail.com>
2014-05-31 09:08:36 -0700

    Updated Version.

--- Plugins/UI/StandardSequence/AssemblyInfo.cs
+++ Plugins/UI/StandardSequence/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.2.150.2")]
-[assembly: AssemblyFileVersion("0.2.150.2")]
+[assembly: AssemblyVersion("0.2.151.1")]
+[assembly: AssemblyFileVersion("0.2.151.1")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 [assembly: ComVisible(false)]
 [assembly: AssemblyTrademark("")]
--- fmod/AssemblyInfo.cs
+++ fmod/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 // Assembly fmod, Version 1.0.0.0
 
-[assembly: System.Reflection.AssemblyVersion("0.2.150.2")]
+[assembly: System.Reflection.AssemblyVersion("0.2.151.1")]
 [assembly: System.Reflection.AssemblyTitle("fmod")]
 [assembly: System.Reflection.AssemblyDescription("FMOD interface for Vixen")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
@@ -10,7 +10,7 @@
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("69a72273-4e43-4617-a8bf-0ce05435efe3")]
-[assembly: System.Reflection.AssemblyFileVersion("0.2.150.2")]
+[assembly: System.Reflection.AssemblyFileVersion("0.2.151.1")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
