a823a2935ff3aa387ba36399057cb0df93a780b8
John McAdams <macebobo@gmail.com>
2015-01-26 16:33:00 -0800

    Version 0.3.26.1

--- fmod/AssemblyInfo.cs
+++ fmod/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.3.25.1")]
+[assembly: AssemblyVersion("0.3.26.1")]
 [assembly: AssemblyTitle("fmod")]
 [assembly: AssemblyDescription("FMOD interface for Vixen")]
 [assembly: AssemblyConfiguration("")]
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("69a72273-4e43-4617-a8bf-0ce05435efe3")]
-[assembly: AssemblyFileVersion("0.3.25.1")]
+[assembly: AssemblyFileVersion("0.3.26.1")]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.EnableEditAndContinue | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.Default)]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
