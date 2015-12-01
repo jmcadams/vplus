c4f2b3d87ad12b70c9a8bd392bded6c6730a4584
John McAdams <macebobo@gmail.com>
2015-11-30 20:29:28 -0800

    VIX-63 Release

--- Plugins/Output/SeqIOHelpers/Properties/AssemblyInfo.cs
+++ Plugins/Output/SeqIOHelpers/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.3.319.1")]
-[assembly: AssemblyFileVersion("0.3.319.1")]
+[assembly: AssemblyVersion("0.3.333.83")]
+[assembly: AssemblyFileVersion("0.3.333.83")]
 [assembly: AssemblyCopyright("Copyleft 2013-2015")]
 [assembly: AssemblyTrademark("No Trademarks Claimed or Implied")]
 [assembly: AssemblyProduct("VixenPlus")]
